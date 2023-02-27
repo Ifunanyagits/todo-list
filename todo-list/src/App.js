6470a1d1b52fb18e11be4b9429c15a66a08d930d
Ifunanyagits <iloifunanya18@gmail.com>
2023-02-27 09:17:58 +0100

    modified app.css and improved style

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -23,22 +23,25 @@ const TodoList = () => {
   };
 
   return (
-    <div className="container">
-      <div className="todo-list">
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={newTodo} onChange={handleChange} />
-          <button type="submit">Add Todo</button>
-        </form>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={todo} className="todo-item">
-              {todo}
-              <button type="button" onClick={() => handleDelete(index)}>
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
+    <div>
+        <h2>My to-do list</h2>
+      <div className="container">
+        <div className="todo-list">
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Add your to-do' value={newTodo} onChange={handleChange} required/>
+            <button type="submit">Add Todo</button>
+          </form>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={todo} className="todo-item">
+                {todo}
+                <button className='deleteItem' type="button" onClick={() => handleDelete(index)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
